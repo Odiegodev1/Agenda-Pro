51c7a7ef0649f169b51ad4ede37646fbfc3e2737
Odiegodev1 <odiegodev10@gmail.com>
2026-01-09 15:25:45 -0300

    verce--1l

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,7 @@
-import { PrismaClient } from "@prisma/client"
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
-
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["query", "error"],
-  })
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma
-}
+import { PrismaClient } from "./generated/prisma/client"
+ 
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+ 
+export const prisma = globalForPrisma.prisma || new PrismaClient()
+ 
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
