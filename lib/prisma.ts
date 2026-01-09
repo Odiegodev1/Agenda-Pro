d33a6d541fb7f11c5b4c813cbf8b2543b496ed11
Odiegodev1 <odiegodev10@gmail.com>
2026-01-09 15:16:10 -0300

    vercel

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,14 @@
-import { PrismaClient } from "./generated/prisma"
+import { PrismaClient } from "@prisma/client"
 
-import { PrismaPg } from "@prisma/adapter-pg"
-
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!
-})
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
 
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({ adapter })
+  new PrismaClient({
+    log: ["query", "error"],
+  })
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma
