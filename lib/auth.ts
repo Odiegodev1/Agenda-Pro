3f161b6b85c5c293f30b1d0df7fdb53e01f4e2d0
Odiegodev1 <odiegodev10@gmail.com>
2026-01-09 15:56:11 -0300

    fix: prisma client import for vercel build

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,12 +1,13 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import {prisma} from "./prisma";
+import { prisma } from "./prisma";
+
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   secret: process.env.AUTH_SECRET!,
   trustHost: true,
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma as any),
   providers: [
     GitHub({
       clientId: process.env.AUTH_GITHUB_ID!,
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,16 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "./generated/prisma"
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined
-}
+import { PrismaPg } from "@prisma/adapter-pg"
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!
+})
 
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["query", "error"],
-  })
+  new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma
