4bf45742b3acd7e387e334084e5fa9e190f209f6
Odiegodev1 <odiegodev10@gmail.com>
2026-01-09 15:46:38 -0300

    verce-5

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "./prisma";
+import {prisma} from "./prisma";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   secret: process.env.AUTH_SECRET!,
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,19 +1,25 @@
+import { PrismaClient } from "@prisma/client"
+import { PrismaPg } from "@prisma/adapter-pg"
+import { Pool } from "pg"
 
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from './generated/prisma'
-
-const globalForPrisma = global as unknown as {
-    prisma: PrismaClient
-}
-
-const adapter = new PrismaPg({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 })
 
-const prisma = globalForPrisma.prisma || new PrismaClient({
-  adapter,
-})
+const adapter = new PrismaPg(pool)
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
 
-export default prisma
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  })
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma
+}
+
+
