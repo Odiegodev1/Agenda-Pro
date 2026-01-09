bce00f01b2330bc3f716d8415e4a3bda5cf603ae
Odiegodev1 <odiegodev10@gmail.com>
2026-01-09 15:37:49 -0300

    verce-4

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,23 +1,19 @@
-import { PrismaClient } from "@prisma/client"
-import { PrismaPg } from "@prisma/adapter-pg"
-import { Pool } from "pg"
 
-const pool = new Pool({
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from './generated/prisma'
+
+const globalForPrisma = global as unknown as {
+    prisma: PrismaClient
+}
+
+const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 })
 
-const adapter = new PrismaPg(pool)
+const prisma = globalForPrisma.prisma || new PrismaClient({
+  adapter,
+})
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    adapter,
-  })
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma
-}
+export default prisma
